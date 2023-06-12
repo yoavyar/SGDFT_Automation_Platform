0e149b2cbd33ed89f9b8664b94086ba7855741a0
yoavyar <130863042+yoavyar@users.noreply.github.com>
2023-06-12 14:28:11 +0300

    Update inside_cheetah_script.sh

--- inside_cheetah_script.sh
+++ inside_cheetah_script.sh
@@ -3,7 +3,7 @@
 source **
 setenv PATH **/${PATH}
 setenv NB_SIGKILL_TIMEOUT 720
-**/spgdft_collaterals.pl $1 $2 $3 $4                #Nickolay's script $1 - partition name, $2 - syn area,  $3 - Compiler,  $4 - Netlist path  
+**/spgdft_collaterals.pl $1 $2 $3 $4                #Perl script $1 - partition name, $2 - syn area,  $3 - Compiler,  $4 - Netlist path  
 
 #cat /**/waivers_folder/waivers/block_waiver.$block.swl >> $ward/runs/$block/$tech/sgdft_netlist_drc/scripts/block_waiver.swl  ##for manual waivers
 #cat /**/waivers_folder/vars/vars.$block.tcl >> $ward/runs/$block/$tech/sgdft_netlist_drc/scripts/vars.tcl				    	##for manual waivers
